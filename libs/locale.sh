98abe1c486e3be7c2b1ad197a1c84e87c602fbb3
Yin Zhang <14476+melcloud@users.noreply.github.com>
2022-06-11 12:06:21 +1000

    Enable en_GB for fallback

--- libs/locale.sh
+++ libs/locale.sh
@@ -3,6 +3,7 @@
 function setup_locale() {
 	info_log "Setup locale"
 	sed -i -e '/^#en_AU.UTF-8/s/^#//' /mnt/etc/locale.gen
+	sed -i -e '/^#en_GB.UTF-8/s/^#//' /mnt/etc/locale.gen
 	sed -i -e '/^#zh_CN.UTF-8/s/^#//' /mnt/etc/locale.gen
 
 	cat<<EOF > /mnt/etc/locale.conf
